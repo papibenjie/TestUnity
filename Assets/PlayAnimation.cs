87caed1060fa6bd3a3529806396de91a6da5692d
Marc-Antoine Quessy <papibenjie@gmail.com>
2018-02-06 12:36:27 -0800

    allo

---
+++ Assets/PlayAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour {
+
+
+	public AnimationClip anim_to_play;
+	Animation anim;
+	Animator an;
+
+	void Start() {
+		anim = GetComponent<Animation>();
+	}
+
+	void Update () {
+		if (Input.GetKey(KeyCode.W)){
+			anim.clip = anim_to_play;
+			anim.Play();        
+
+		}
+	}
+}
